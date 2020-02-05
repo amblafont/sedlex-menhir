ea91c2710527718704f9e8ea374bc900181fb4ea
sd <ze>
2020-02-06 08:05:54 +1100

    ca marche

--- sedlex-menhir/build.sh
+++ sedlex-menhir/build.sh
@@ -2,8 +2,8 @@
 
 # menhir
 menhir --ocamlc 'ocamlfind ocamlc -thread -package sedlex -package menhirLib' --explain --infer parser.mly
-# sedlex
-ocamlfind ocamlc -c -package sedlex.ppx lexer.ml
 ocamlc -c parser.mli
 ocamlc -c parser.ml
+# sedlex
+ocamlfind ocamlc -c -package sedlex.ppx lexer.ml
 ocamlfind ocamlc -o calc -linkpkg -package sedlex -package menhirLib lexer.cmo parser.cmo calc.ml
